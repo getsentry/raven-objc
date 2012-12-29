b7d0d8543529aa27b628be54af9a7d8b302941c2
David Cramer <dcramer@gmail.com>
2012-12-28 16:53:49 -0800

    Additional DSN parsing tests

--- RavenTests/RavenConfigTest.m
+++ RavenTests/RavenConfigTest.m
@@ -27,4 +27,37 @@
     STAssertTrue([[config.serverURL absoluteString] isEqualToString:expectedURL], @"Got incorrect serverURL %@", [config.serverURL absoluteString]);
 }
 
+- (void)testSetDSNWithSSLPortUndefined
+{
+    RavenConfig *config = [RavenConfig alloc];
+    BOOL didParse = [config setDSN:@"https://public_key:secret_key@example.com/project-id"];
+    
+    STAssertTrue(didParse, @"Failed to parse DSN");
+    
+    STAssertTrue([config.publicKey isEqualToString:@"public_key"], @"Got incorrect publicKey %@", config.publicKey);
+    STAssertTrue([config.secretKey isEqualToString:@"secret_key"], @"Got incorrect secretKey %@", config.secretKey);
+    STAssertTrue([config.projectId isEqualToString:@"project-id"], @"Got incorrect projectId %@", config.projectId);
+    
+    NSString *expectedURL = @"https://example.com:443/api/project-id/store/";
+    
+    STAssertTrue([[config.serverURL absoluteString] isEqualToString:expectedURL], @"Got incorrect serverURL %@", [config.serverURL absoluteString]);
+}
+
+- (void)testSetDSNWithoutSSLPortUndefined
+{
+    RavenConfig *config = [RavenConfig alloc];
+    BOOL didParse = [config setDSN:@"http://public_key:secret_key@example.com/project-id"];
+    
+    STAssertTrue(didParse, @"Failed to parse DSN");
+    
+    STAssertTrue([config.publicKey isEqualToString:@"public_key"], @"Got incorrect publicKey %@", config.publicKey);
+    STAssertTrue([config.secretKey isEqualToString:@"secret_key"], @"Got incorrect secretKey %@", config.secretKey);
+    STAssertTrue([config.projectId isEqualToString:@"project-id"], @"Got incorrect projectId %@", config.projectId);
+    
+    NSString *expectedURL = @"http://example.com:80/api/project-id/store/";
+    
+    STAssertTrue([[config.serverURL absoluteString] isEqualToString:expectedURL], @"Got incorrect serverURL %@", [config.serverURL absoluteString]);
+}
+
+
 @end
