742c521aadb7eee1eb800b5d2d99dd791d75a46d
Dmitry Vorobyov <d@dvor.me>
2014-02-10 17:01:59 +0200

    RavenClient: making comments look nice

--- Raven/RavenClient.h
+++ Raven/RavenClient.h
@@ -24,12 +24,14 @@ typedef enum {
 @property (strong, nonatomic) NSDictionary *extra;
 @property (strong, nonatomic) NSDictionary *tags;
 
-// By setting tags with setTags: selector it will also set default settings:
-// - Build version
-// - OS version (on iOS)
-// - Device model (on iOS)
-//
-// For full control use this method.
+/**
+ * By setting tags with setTags: selector it will also set default settings:
+ * - Build version
+ * - OS version (on iOS)
+ * - Device model (on iOS)
+ *
+ * For full control use this method.
+ */
 - (void)setTags:(NSDictionary *)tags withDefaultValues:(BOOL)withDefaultValues;
 
 // Singleton and initializers
@@ -42,12 +44,14 @@ typedef enum {
 - (id)initWithDSN:(NSString *)DSN extra:(NSDictionary *)extra;
 - (id)initWithDSN:(NSString *)DSN extra:(NSDictionary *)extra tags:(NSDictionary *)tags;
 
-// Messages
-//
-// All entries from additionalExtra/additionalTags are added to extra/tags.
-//
-// If dictionaries contain the same key, the entries from extra/tags dictionaries will be replaced with entries
-// from additionalExtra/additionalTags dictionaries.
+/**
+ * Messages
+ *
+ * All entries from additionalExtra/additionalTags are added to extra/tags.
+ *
+ * If dictionaries contain the same key, the entries from extra/tags dictionaries will be replaced with entries
+ * from additionalExtra/additionalTags dictionaries.
+ */
 - (void)captureMessage:(NSString *)message;
 - (void)captureMessage:(NSString *)message level:(RavenLogLevel)level;
 - (void)captureMessage:(NSString *)message level:(RavenLogLevel)level method:(const char *)method file:(const char *)file line:(NSInteger)line;
@@ -61,12 +65,14 @@ typedef enum {
                   file:(const char *)file
                   line:(NSInteger)line;
 
-// Exceptions
-//
-// All entries from additionalExtra/additionalTags are added to extra/tags.
-//
-// If dictionaries contain the same key, the entries from extra/tags dictionaries will be replaced with entries
-// from additionalExtra/additionalTags dictionaries.
+/**
+ * Exceptions
+ *
+ * All entries from additionalExtra/additionalTags are added to extra/tags.
+ *
+ * If dictionaries contain the same key, the entries from extra/tags dictionaries will be replaced with entries
+ * from additionalExtra/additionalTags dictionaries.
+ */
 - (void)captureException:(NSException *)exception;
 - (void)captureException:(NSException *)exception sendNow:(BOOL)sendNow;
 - (void)captureException:(NSException *)exception additionalExtra:(NSDictionary *)additionalExtra additionalTags:(NSDictionary *)additionalTags sendNow:(BOOL)sendNow;
