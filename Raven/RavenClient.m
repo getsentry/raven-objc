075328b25c842220d924dc879de9d183d06c22e5
Tommy Mikalsen <tommy@mikalsen.it>
2014-11-13 22:30:37 +0100

    Bumped version to 0.5.0

--- Raven/RavenClient.m
+++ Raven/RavenClient.m
@@ -21,7 +21,7 @@ NSString *const kRavenLogLevelArray[] = {
 
 NSString *const userDefaultsKey = @"nl.mixedCase.RavenClient.Exceptions";
 NSString *const sentryProtocol = @"4";
-NSString *const sentryClient = @"raven-objc/0.4.0";
+NSString *const sentryClient = @"raven-objc/0.5.0";
 
 static RavenClient *sharedClient = nil;
 
