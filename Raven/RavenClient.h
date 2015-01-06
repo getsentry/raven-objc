db32a6b5fecb8c8df156ecde984a859bff9c9c15
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2015-01-06 11:32:58 +0900

    Fixed compilation error
    RavenClient uses UIDevice, but did not import UIKit.

--- Raven/RavenClient.h
+++ Raven/RavenClient.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 Gangverk. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #define RavenCaptureMessage( s, ... ) [[RavenClient sharedClient] captureMessage:[NSString stringWithFormat:(s), ##__VA_ARGS__] level:kRavenLogLevelDebugInfo method:__FUNCTION__ file:__FILE__ line:__LINE__]
 
