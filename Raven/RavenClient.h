0a83a0bd8e6b8eae574ab7e47648179f41adf50b
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2015-01-08 07:55:28 +0900

    Unbroken OS X build

--- Raven/RavenClient.h
+++ Raven/RavenClient.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 Gangverk. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 #define RavenCaptureMessage( s, ... ) [[RavenClient sharedClient] captureMessage:[NSString stringWithFormat:(s), ##__VA_ARGS__] level:kRavenLogLevelDebugInfo method:__FUNCTION__ file:__FILE__ line:__LINE__]
 
