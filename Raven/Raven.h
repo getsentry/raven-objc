c132047e03cb0931db630c8822140c46ad191811
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2015-01-20 13:06:48 +0900

    Added iOS Framework target

---
+++ Raven/Raven.h
@@ -0,0 +1,13 @@
+//
+//  Raven.h
+//  Raven
+//
+//  Created by Fjölnir Ásgeirsson on 1/20/15.
+//  Copyright (c) 2015 Gangverk. All rights reserved.
+//
+
+#import <Raven/RavenClient.h>
+#import <Raven/RavenConfig.h>
+
+FOUNDATION_EXPORT double RavenVersionNumber;
+FOUNDATION_EXPORT const unsigned char RavenVersionString[];
