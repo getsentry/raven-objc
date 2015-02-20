b27609029aa11108d7f1a9e05432e5f849f445a8
Aleksandr Dodatko <a.dodatko@healthjoy.com>
2015-02-20 11:50:31 +0200

    [carthage] Added a static lib and framework target

---
+++ Raven/Raven.h
@@ -0,0 +1,21 @@
+//
+//  Raven.h
+//  Raven
+//
+//  Created by Oleksandr Dodatko on 2/20/15.
+//  Copyright (c) 2015 Gangverk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Raven.
+FOUNDATION_EXPORT double RavenVersionNumber;
+
+//! Project version string for Raven.
+FOUNDATION_EXPORT const unsigned char RavenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Raven/PublicHeader.h>
+
+
+#import <Raven/RavenClient.h>
+#import <Raven/RavenConfig.h>
